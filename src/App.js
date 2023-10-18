59683baa7012b18422c18e43f4e0fc506fb8c6ab
Anish Mandal <anish.mandal@bitskraft.com>
2023-10-18 15:05:29 +0545

    naxa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchDataRequest } from './redux/action';
 
 function App() {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.data);
+  const error = useSelector((state) => state.error);
+
+  useEffect(() => {
+    dispatch(fetchDataRequest());
+  }, [dispatch]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {data.map((item) => (
+        <div key={item.id}>
+        <div>{item.title}</div>
+        <div>{item.description1}</div>
+        <div>{item.description2}</div>
+        <div>
+          <img src={item.photo} alt={item.title} />
+        </div>
+        <div>
+          <img src={item.icon} alt={item.title} />
+        </div>
+        <div>{item.service_order}</div>
+        </div>
+      ))}
     </div>
   );
 }
